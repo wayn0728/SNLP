c9107035d3074cd1354a822be6c15d29ff066963
Jiawei Dong <jwdong890728@gmail.com>
2012-11-11 13:31:28 -0500

    change to 1000

--- NLP class/nlp/assignment5/PCFGParserTester.java
+++ NLP class/nlp/assignment5/PCFGParserTester.java
@@ -669,7 +669,7 @@ public class PCFGParserTester {
 	    String basePath = ".";
 	    boolean verbose = false;
 	    String testMode = "validate";
-	    int maxTrainLength =100;
+	    int maxTrainLength =1000;
 	    int maxTestLength = 40;
 
 	    // Update defaults using command line specifications
